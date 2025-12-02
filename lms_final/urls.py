313503d0f706c1991b39c40d646062e2280b74c5
DfsSpooky <93174212+DfsSpooky@users.noreply.github.com>
2025-12-01 21:46:05 -0500

    Update urls.py

--- lms_final/urls.py
+++ lms_final/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views 
+from academy.views import current_user
 from rest_framework.routers import DefaultRouter
 from academy.views import CourseViewSet, ProgressViewSet, current_user
 from django.contrib.auth.views import LogoutView
@@ -19,4 +21,22 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     # path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    # --- AGREGA ESTAS RUTAS DE RECUPERACIÓN ---
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), 
+         name='password_reset'),
+         
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), 
+         name='password_reset_done'),
+         
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+         
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), 
+         name='password_reset_complete'),
+    # -------------------------------------------
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
